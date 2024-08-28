c34b9402b58dd0eb2e7ac04b018752f1cf24802b
Gesang Widigdo <gesangwidigdo@gmail.com>
2024-08-28 08:49:16 +0700

    feat: create many2many for product_transaction model

---
+++ models/productTransactionModel.go
@@ -0,0 +1,19 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type ProductTransaction struct {
+	gorm.Model
+	Transaction_id uint    `gorm:"primaryKey;autoIncrement:false"`
+	Product_id     uint    `gorm:"primaryKey;autoIncrement:false"`
+	Quantity       int     `gorm:"not null; type:integer"`
+	Total          float64 `gorm:"not null; type:decimal; default:0"`
+
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt
+}
