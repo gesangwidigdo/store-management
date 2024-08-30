5350264c902ef4e9f5b31ea8b9e42668c78742a2
Gesang Widigdo <gesangwidigdo@gmail.com>
2024-08-31 04:16:11 +0700

    feat: create product custom response

---
+++ utils/customResponse/product.go
@@ -0,0 +1,17 @@
+package customresponse
+
+import "github.com/gesangwidigdo/store-management/models"
+
+type ProductResponse struct {
+	ID           uint    `json:"id"`
+	Product_name string  `json:"product_name"`
+	Price        float64 `json:"price"`
+}
+
+func ToProductResponse(product models.Product) ProductResponse {
+	return ProductResponse{
+		ID:           product.ID,
+		Product_name: product.Product_name,
+		Price:        product.Price,
+	}
+}
